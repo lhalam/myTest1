910f5a1d2668d15c7288cbe7f31001741c2b4ec2
lhalam <galamagal86@gmail.com>
2013-03-12 02:32:36 +0200

    mycommit2

---
+++ MyTestProject2/src/myclass.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+
+
+public class myclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
